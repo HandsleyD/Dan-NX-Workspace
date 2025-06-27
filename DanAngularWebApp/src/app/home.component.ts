070f01be85da203cf761cda07652fafd5ae3727a
Daniel Handsley <daniel.handsley@gmail.com>
2025-06-27 22:15:39 +0100

    Remove unused items

--- DanAngularWebApp/src/app/home.component.ts
+++ DanAngularWebApp/src/app/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, MatButtonModule],
+  imports: [MatButtonModule],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
