fab56f7d31832e7b511e57f78cc5ee0902d2ffbf
Daniel Handsley <daniel.handsley@gmail.com>
2025-06-29 01:01:52 +0100

    Fixing signals and small layout tweaks

--- DanAngularWebApp/src/app/navigation/navigation.component.ts
+++ DanAngularWebApp/src/app/navigation/navigation.component.ts
@@ -1,36 +1,45 @@
-import { Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
     MatListModule,
     MatIconModule,
-    AsyncPipe,
     RouterLink,
+    RouterOutlet,
   ],
 })
 export class NavigationComponent {
-  private breakpointObserver = inject(BreakpointObserver);
+  private readonly breakpointObserver = inject(BreakpointObserver);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
+  readonly isHandset = signal(false);
+
+  constructor() {
+    effect(() => {
+      this.breakpointObserver
+        .observe(Breakpoints.Handset)
+        .subscribe((result) => {
+          this.isHandset.set(result.matches);
+        });
+    });
+  }
 }
