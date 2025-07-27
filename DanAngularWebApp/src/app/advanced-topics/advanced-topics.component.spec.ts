451d3116d5277e9f09b73f8674a4a1c6a4f35235
Daniel Handsley <daniel.handsley@gmail.com>
2025-07-27 02:38:55 +0100

    test: enhance component tests with ActivatedRoute mock and adjust expectations

--- DanAngularWebApp/src/app/advanced-topics/advanced-topics.component.spec.ts
+++ DanAngularWebApp/src/app/advanced-topics/advanced-topics.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { AdvancedTopicsComponent } from './advanced-topics.component';
 
 describe('AdvancedTopicsComponent', () => {
@@ -8,6 +9,16 @@ describe('AdvancedTopicsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AdvancedTopicsComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { params: {} },
+            params: { subscribe: jest.fn() },
+            queryParams: { subscribe: jest.fn() },
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdvancedTopicsComponent);
